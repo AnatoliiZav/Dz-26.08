21a960de9ea3c64b11def0b9784abfd93f4d79a8
Anatolii <zavorinanatolii@mail.ru>
2022-08-27 22:18:48 +0800

    Dz-47

---
+++ Dz-47/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            result[i, j] = new Random().NextDouble() * new Random().Next(minValue, maxValue + 1);
+
+    return result;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]:N1} ");
+        Console.WriteLine();
+    }
+}
+
+Console.Write("m= ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("n= ");
+int n = int.Parse(Console.ReadLine());
+
+double[,] myArray = GetArray(m, n, -10, 10);
+PrintArray(myArray);
+
+
